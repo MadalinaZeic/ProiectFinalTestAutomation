408b871c185c8f1f0fe7e3bea68dd7b2581e816d
Fakeru <cosmin.zeic@gmail.com>
2020-10-09 13:58:32 +0300

    Initial setup

---
+++ src/main/java/org/fasttrackit/pageObjects/Header.java
@@ -0,0 +1,62 @@
+package org.fasttrackit.pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class Header {
+
+
+    public WebElement selectCategory(String categoryName, WebDriver driver) {
+        return driver.findElement(By.xpath("//nav[@id='nav']//a[contains(text(),'" + categoryName + "')]"));
+    }
+
+
+    @FindBy(css = ".mini-products-list .product-name")
+    List<WebElement> productNameInMiniCart;
+
+    @FindBy(className = "header-minicart")
+    private WebElement miniCart;
+
+
+    @FindBy(id="search")
+    private WebElement searchField;
+
+    @FindBy(className = "search-button")
+    private WebElement searchButton;
+
+    public void search(String keyword) {
+        searchField.clear();
+        searchField.sendKeys(keyword);
+        searchButton.click();
+    }
+    @FindBy(xpath = "//div[@class='page-header-container']//img[@class='large']")
+    private WebElement homeLogo;
+
+    public void clickHomeLogo() {
+        homeLogo.click();
+    }
+
+
+
+    public List<WebElement> getProductNameFromMiniCart() {
+        return productNameInMiniCart;
+    }
+    public WebElement getSearchButton() {
+        return searchButton;
+    }
+    public WebElement getSearchField() {
+        return searchField;
+    }
+    public WebElement getHomeLogo() {
+        return homeLogo;
+    }
+    public WebElement getMiniCart() {
+        return miniCart;
+    }
+
+
+}
